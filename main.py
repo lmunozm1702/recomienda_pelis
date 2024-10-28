51536530e19528cdd56f284c07496a8d541e0aef
Luis Muñoz <l.munoz.m@outlook.com>
2024-10-28 13:35:45 -0300

    Agrega endpoint cantidad_filmaciones_dia

--- main.py
+++ main.py
@@ -44,3 +44,33 @@ async def cantidad_filmaciones_mes(mes: int):
     "error": None
   }
 
+#Se ingresa un día en idioma Español. Debe devolver la cantidad de películas 
+#que fueron estrenadas en día consultado en la totalidad del dataset.
+@app.get("/cantidad_filmaciones_dia")
+async def cantidad_filmaciones_mes(dia: int):
+  if dia < 1 or dia > 31:
+    return {"dia": dia, "error": "Día no existe"}
+  
+  #transformar el mes a string de 2 digitos
+  if dia < 10:
+    dia = "0" + str(dia)
+  else:
+    dia = str(dia)
+  
+  selected_movies_columns = ['release_date'] 
+  start = time.perf_counter()  
+  df_movies = pd.read_parquet("datasets/movies_dataset.parquet", columns=selected_movies_columns)
+  end = time.perf_counter()
+
+  sql_result = psql.sqldf("SELECT COUNT(*) FROM df_movies WHERE strftime('%d', release_date) = '{dia}'".format(dia=dia))
+  cantidad_filmaciones_dia = sql_result.values[0][0]
+  
+  return {
+    "message": "{cantidad_filmaciones_dia} películas fueron estrenadas en el día {dia}".format(cantidad_filmaciones_dia=cantidad_filmaciones_dia, dia=dia),
+    "dia": dia,
+    "cantidad_de_filmaciones": cantidad_filmaciones_dia.item(),
+    "tiempo_lectura_parquet_movies": f"{end - start:0.4f} segundos",
+    "tamaño_dataset_movies": f"{sys.getsizeof(df_movies)/1024/1024:0.4f} MB",
+    "error": None
+  }
+
